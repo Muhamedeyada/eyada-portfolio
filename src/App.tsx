e5054934b3be1b28e4260c89e423d19338cc7bd4
mohamed eyada <mohamed.ayada.ma@gmail.com>
2025-04-24 16:41:15 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen p-8">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Hello Tailwind with Vite + React!
+      </h1>{" "}
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
